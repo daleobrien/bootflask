186a1251cabba5d78eec0e9643c541481cd322d1
Dale O'Brien <lost.blue.whale@gmail.com>
2012-12-14 23:24:09 +1100

    Added basic project with login screen and main page

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ login_manager = LoginManager()
 
 login_manager.anonymous_user = Anonymous
 login_manager.login_view = "login"
-login_manager.login_message = u"Please log in to access this page."
+#login_manager.login_message = u"Please log in to access this page."
 login_manager.refresh_view = "reauth"
 
 
@@ -83,8 +83,9 @@ login_manager.setup_app(app)
 
 
 @app.route("/")
+@fresh_login_required
 def index():
-    return render_template("index.html")
+    return render_template("main.html")
 
 
 @app.route("/secret")
@@ -95,32 +96,24 @@ def secret():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
     if request.method == "POST" and "username" in request.form:
+
         username = request.form["username"]
         password = request.form["password"]
+
         if User.check_password(username, password):
+
             remember = request.form.get("remember", "no") == "yes"
+
             if login_user(USER_NAMES[username], remember=remember):
-                flash("Logged in!")
+
                 return redirect(request.args.get("next") or url_for("index"))
-            else:
-                flash("Sorry, but you could not log in.")
-        else:
-            flash(u"Invalid username or password.")
-    return render_template("login.html")
+
+    return render_template("main.html")
 
 
-@app.route("/reauth", methods=["GET", "POST"])
-@login_required
-def reauth():
-    if request.method == "POST":
-        confirm_login()
-        flash(u"Reauthenticated.")
-        return redirect(request.args.get("next") or url_for("index"))
-    return render_template("reauth.html")
-
-
-@app.route("/logout")
+@app.route("/logout", methods=["POST"])
 @login_required
 def logout():
     logout_user()
